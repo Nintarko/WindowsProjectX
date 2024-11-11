6811719fe579b1e4d0add497dbe4c9a42e2e7f0d
Nintarko <156791262+Nintarko@users.noreply.github.com>
2024-11-11 12:21:15 +0300

    Добавил TextBox

--- WindowsProjectX/Form1.Designer.cs
+++ WindowsProjectX/Form1.Designer.cs
@@ -29,34 +29,46 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(358, 150);
+            this.button1.Location = new System.Drawing.Point(350, 112);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 88);
+            this.button1.Size = new System.Drawing.Size(59, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "No touch";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hahahahahaha";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
